86b0071737b98a23b640aee2c5e698f1505e6c7f
AIS <arttuilamarisalo@gmail.com>
2023-12-17 17:24:39 +0200

    fortnite

--- get_next_line/gnl_tester/files/generate_fortnite.py
+++ get_next_line/gnl_tester/files/generate_fortnite.py
@@ -1,7 +1,8 @@
-'''
+
 with open("fortnite_mega.txt", "w") as file:
-    for _ in range(1000000):
+    for _ in range(100000):
         file.write("fortnite " * 10 + "\n")
 '''
 with open("fortnite_mega_oneline.txt", "w") as file:
     file.write(" ".join(["fortnite"] * 100000))
+'''
